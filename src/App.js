f29242c83ad156ff903de5fc5181877165c2cd2c
ejaramillom <ejaramilo@hotmail.com>
2019-11-17 15:51:47 -0500

    corrected functionality

--- src/App.js
+++ src/App.js
@@ -6,50 +6,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      items: [ {text:"Sacar la ropa",key: 0},
-               {text: "Hacer la cama", key: 1},
-               {text: "Leer un rato",  key: 2} ]
+      value: "",
+      items: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.createTasks = this.createTasks.bind(this);
   }
   handleSubmit(event) {
-    if (this._inputElement.value !== '') {
-      var newItem = {
-        text : this._inputElement.value,
-        key: Date.now()
-      };
-    this.setState((prevState) => {
+    const newItem = {
+      text: this.state.value,
+    };
+    this.setState(() => {
       return ({
-        items: prevState.items.concat(newItem),
+        items:  this.state.items.concat(newItem),
+        value: ""
       });
     });
-    this._inputElement.value = "";
-    }
     event.preventDefault();
   }
   handleChange(event) {
+    this.setState({value: event.target.value});
     event.preventDefault();
-   this.setState({value: event.target.value});
   }
   createTasks(item) {
-    return (<li key={item.key}>{item.text}</li>)
+    return (<li>{item.text}</li>)
   }
   render() {
-    const todoEntries = this.state.items;
-    const listItems = todoEntries.map(this.createTasks);
+    const listItems = this.state.items.map(this.createTasks);
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
-          <ul className="todo" entries={this.state.items}>
+          <ul className="todo">
+            <li>Sacar la ropa</li>
+            <li>Hacer la cama</li>
+            <li>Leer un rato</li>
             {listItems}
           </ul>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" ref={(a) => this._inputElement = a} id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-          </form>
+           <form onSubmit={this.handleSubmit}>
+             <input type="text" id="new-task" value={this.state.value} onChange={this.handleChange} placeholder="Ingresa una tarea y oprime Enter" />
+           </form>
         </div>
       </div>
     )
